0c4f54e9fe8cb60c5522aac59e5fbceb0735b109
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-02-20 16:54:59 +0100

    Adds some nice utils

---
+++ utils.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func filenameWithSuffix(fpath string, iter uint) string {
+	res := strings.TrimSuffix(fpath, filepath.Ext(fpath))
+	curIter, _ := strconv.Atoi(strings.TrimPrefix(filepath.Ext(res), "."))
+	if curIter != 0 {
+		res = strings.TrimSuffix(res, filepath.Ext(res))
+	}
+
+	if iter == 0 {
+		return res + filepath.Ext(fpath)
+	}
+
+	return res + "." + strconv.FormatUint(uint64(iter), 10) + filepath.Ext(fpath)
+}
+
+func CreateFileWithoutOverwrite(fpath string) (*os.File, string, error) {
+	iter := uint(0)
+	for {
+		toTest := filenameWithSuffix(fpath, iter)
+		if _, err := os.Stat(toTest); os.IsNotExist(err) {
+			f, err := os.Create(toTest)
+			return f, toTest, err
+		}
+		iter += 1
+	}
+}
---
+++ utils_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	. "gopkg.in/check.v1"
+)
+
+type UtilsSuite struct {
+	tmpDir string
+}
+
+var _ = Suite(&UtilsSuite{})
+
+func (s *UtilsSuite) SetUpSuite(c *C) {
+	var err error
+	s.tmpDir, err = ioutil.TempDir("", "dieu-utils-tests")
+
+	c.Check(err, IsNil)
+}
+
+func (s *UtilsSuite) TearDownSuite(c *C) {
+	c.Check(os.RemoveAll(s.tmpDir), IsNil)
+}
+
+func (s *UtilsSuite) TestNameSuffix(c *C) {
+	testdata := []struct {
+		Base     string
+		i        uint
+		Expected string
+	}{
+		{"out.txt", 0, "out.txt"},
+		{"bar.foo.2.txt", 3, "bar.foo.3.txt"},
+		{"../some/path/out.42.txt", 2, "../some/path/out.2.txt"},
+		{"../some/path/out.42.txt", 0, "../some/path/out.txt"},
+	}
+
+	for _, d := range testdata {
+		c.Check(filenameWithSuffix(d.Base, d.i), Equals, d.Expected)
+	}
+}
+
+func (s *UtilsSuite) TestCreateWithoutOverwrite(c *C) {
+	files := []string{"out.txt", "out.1.txt", "out.3.txt"}
+
+	for _, f := range files {
+		ff, err := os.Create(filepath.Join(s.tmpDir, f))
+		c.Assert(err, IsNil)
+		defer ff.Close()
+	}
+
+	ff, name, err := CreateFileWithoutOverwrite(filepath.Join(s.tmpDir, files[0]))
+	c.Check(err, IsNil)
+	defer ff.Close()
+	c.Assert(name, Equals, filepath.Join(s.tmpDir, "out.2.txt"))
+	_, err = os.Stat(name)
+	c.Check(err, IsNil)
+
+}
