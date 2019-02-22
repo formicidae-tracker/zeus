3caf6185e4fe27e42c8b9beb21f4626a313ae2ab
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-02-22 16:52:03 +0100

    Mores info

--- README.md
+++ README.md
@@ -10,3 +10,56 @@ the climate for the tracking chamber. It features:
     the chamber enough to match the desired climatic conditions
   * When hardware issue may prevent the system to reach its target
     (water tank empty, faulty fan, electronic device disconnected)
+* YAML based configuration file for setting up the climate state
+  machine
+
+## Getting started
+
+### Prerequesite
+
+If actual climate should be controlled the host should run Linux and
+have installed `slcan-utils` packages.
+
+Otherwise if just simulation is required, you only need to install go
+version 1.11. You can follow installation instructions
+[here](https://golang.org/doc/install).
+
+Please do not forget to setup your GOPATH and [test your
+installation](https://golang.org/doc/install#testing)
+
+### Installing
+
+``` bash
+go get -u git.tuleu.science/fort/dieu
+go install git.tuleu.science
+```
+
+You should now be able to run the `dieu` command.
+
+
+## Testing a season file
+
+You can find sample season file in `examples/` subfolder. You can test
+the climate it will produce using `dieu simulate` subcommand.
+
+``` bash
+cd examples
+dieu -c simple.season simulate [-d 10] [-s 06:00]
+```
+
+Will list on stdout the states dieu will go through over a duration of
+7 days starting from the current time. You can change this duration
+using the `-d` flags and defines the starting time of the simulation
+with the `-s` flags.
+
+
+You can find more information in [examples/list.md].
+
+
+## Authors
+
+  * Alexandre Tuleu - Initial Work
+
+## License
+
+This project is licensed under the GPL version 3 license
--- simulate.go
+++ simulate.go
@@ -6,8 +6,8 @@ import (
 )
 
 type SimulateCommand struct {
-	StartTime time.Time `long:"start-time" short:"s" description:"starting time of the simulation, using current time if left blank"`
-	Duration  int       `long:"duration" short:"d" description:"length of the simulation in days" default:"7"`
+	StartTime string `long:"start-time" short:"s" description:"starting hours and minutes of the simulation like 15:04, using current time if left blank"`
+	Duration  int    `long:"duration" short:"d" description:"length of the simulation in days" default:"7"`
 }
 
 var simulateCommand = SimulateCommand{}
@@ -18,26 +18,35 @@ func (s *SimulateCommand) Execute(args []string) error {
 		return err
 	}
 
-	if s.StartTime.IsZero() == true {
-		s.StartTime = time.Now()
+	var start time.Time
+	if len(s.StartTime) == 0 {
+		start = time.Now()
+	} else {
+		var err error
+		start, err = time.Parse("15:04", s.StartTime)
+		if err != nil {
+			return err
+		}
+		y, m, d := time.Now().Date()
+		start = start.AddDate(y, int(m), d)
 	}
 
 	for n, z := range c.Zones {
-		fmt.Printf("=== Simulating zone '%s' for %d day from %s ===\n", n, s.Duration, s.StartTime.Format("Mon Jan 02 15:04:05 -0700 MST 2006"))
+		fmt.Printf("=== Simulating zone '%s' for %d day from %s ===\n", n, s.Duration, start.Format("Mon Jan 02 15:04:05 -0700 MST 2006"))
 
-		i, err := NewClimateInterpoler(z.States, z.Transitions, s.StartTime.UTC())
+		i, err := NewClimateInterpoler(z.States, z.Transitions, start.UTC())
 		if err != nil {
 			return err
 		}
 		var t time.Time
-		for t = s.StartTime; t.Before(s.StartTime.AddDate(0, 0, s.Duration)); {
+		for t = start; t.Before(start.AddDate(0, 0, s.Duration)); {
 			toTest := t.Add(1 * time.Second)
 			inter := i.CurrentInterpolation(toTest)
 			fmt.Printf("%s state is %s\n", t.Local().Format("Mon Jan 02 15:04:05 -0700 MST 2006"), inter)
 			next, ok := i.NextInterpolationTime(toTest)
 			if ok == false {
 				fmt.Printf("No more transition\n")
-				t = s.StartTime.AddDate(0, 0, s.Duration)
+				t = start.AddDate(0, 0, s.Duration)
 				continue
 			}
 			t = next
