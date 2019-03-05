7ad26479d4a6319ca617b99d0733b8390e972d82
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-03-05 18:42:21 +0100

    Adds a tool to change device

--- tools/arke-id-change/main.go
+++ tools/arke-id-change/main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strings"
+
+	"git.tuleu.science/fort/libarke/src-go/arke"
+
+	"github.com/atuleu/golang-socketcan"
+	"github.com/jessevdk/go-flags"
 )
 
 type Options struct {
 	Interface string `long:"interface" short:"i" description:"interface to use" default:"slcan0"`
 	Class     string `long:"class" short:"c" description:"class of device to change"`
 	Original  uint8  `long:"original" short:"o" description:"orginal ID to change" default:"0"`
-	Original  target `long:"original" short:"o" description:"orginal ID to change" default:"0"`
+	Target    uint8  `long:"target" short:"t" description:"target ID" default:"0"`
 }
 
 func Execute() error {
-	return nil
+	opts := Options{}
+	if _, err := flags.Parse(&opts); err != nil {
+		return err
+	}
+
+	class := map[string]arke.NodeClass{
+		"zeus":    arke.ZeusClass,
+		"celaeno": arke.CelaenoClass,
+		"helios":  arke.HeliosClass,
+	}
+
+	c, ok := class[strings.ToLower(opts.Class)]
+	if ok == false {
+		return fmt.Errorf("Unknown class '%s'", opts.Class)
+	}
+
+	intf, err := socketcan.NewRawInterface(opts.Interface)
+	if err != nil {
+		return err
+	}
+
+	return arke.SendIDChangeRequest(intf, c, arke.NodeID(opts.Original), arke.NodeID(opts.Target))
 }
 
 func main() {
