3c07e2545087d743130e9b43b32937e3922f6049
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-03-11 13:39:31 +0100

    Adds repsonsive charts

--- olympus/webapp/src/app/climate-chart/climate-chart.component.ts
+++ olympus/webapp/src/app/climate-chart/climate-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ElementRef,ViewChild} from '@angular/core';
+import ResizeObserver from 'resize-observer-polyfill';
 import { Chart } from 'chart.js'
 
 
@@ -7,15 +8,69 @@ import { Chart } from 'chart.js'
   templateUrl: './climate-chart.component.html',
   styleUrls: ['./climate-chart.component.css']
 })
-export class ClimateChartComponent implements OnInit {
+export class ClimateChartComponent implements AfterViewInit {
 
-    @ViewChild('climateChart') climateChartRef: ElementRef;
-    chart: Chart;
+    canvas: any;
+	ctx: any;
+	chart : any;
 
-    constructor() { }
+	@ViewChild('climateChartMonitor')
+	public monitor: ElementRef
 
-    ngOnInit() {
+
+	constructor() { }
+
+	ngAfterViewInit() {
+		let ro = new ResizeObserver(entries => {
+			for ( let e of entries) {
+				const cr = e.contentRect;
+				this.chart.options.width = cr.width;
+				this.chart.options.height = cr.height;
+				this.chart.resize();
+			}
+		});
+		ro.observe(this.monitor.nativeElement);
+		this.canvas = document.getElementById('climateChart');
+		this.ctx = this.canvas.getContext('2d');
+		this.chart = new Chart(this.ctx,{
+			type: 'line',
+			data: {
+				labels: ['0','1','2','3','4'],
+				datasets: [
+					{
+						label: 'Humidity',
+						fill: false,
+						lineTension: 0,
+						data: [
+							{x:0,y:40},
+							{x:1,y:42},
+							{x:2,y:38},
+							{x:3,y:50},
+							{x:4,y:50.2}
+						]
+					},
+					{
+						label: 'Temperature',
+						fill: false,
+						lineTension: 0,
+						data: [
+							{x:0,y:20},
+							{x:1,y:20.3},
+							{x:2,y:19.8},
+							{x:3,y:21.2},
+							{x:4,y:20.8}
+						]
+					}
+				],
+			},
+			options: {
+				responsive: false,
+			}
+
+		});
         //todo display a chart
     }
+
+
 
 }
