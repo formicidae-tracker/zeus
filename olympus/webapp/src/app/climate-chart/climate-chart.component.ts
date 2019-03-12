ecac76bb7536995dace4c836b96fb2fe6268b441
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-03-12 16:48:35 +0100

    Improves the climate chart rendering

--- olympus/webapp/src/app/climate-chart/climate-chart.component.ts
+++ olympus/webapp/src/app/climate-chart/climate-chart.component.ts
@@ -51,13 +51,14 @@ export class ClimateChartComponent implements AfterViewInit,OnInit {
 		this.canvas = document.getElementById('climateChart');
 		this.ctx = this.canvas.getContext('2d');
 		this.chart = new Chart(this.ctx,{
-			type: 'line',
+			type: 'scatter',
 			data: {
-				labels: ['0','1','2','3','4'],
 				datasets: [
 					{
+						borderColor: '#1f77b4',
 						label: 'Humidity',
 						fill: false,
+						showLine: true,
 						lineTension: 0,
 						data: [
 							{x:0,y:40},
@@ -65,11 +66,14 @@ export class ClimateChartComponent implements AfterViewInit,OnInit {
 							{x:2,y:38},
 							{x:3,y:50},
 							{x:4,y:50.2}
-						]
+						],
+						yAxisID: 'y-humidity'
 					},
 					{
-						label: 'Temperature',
+						label: 'Temperature Ant',
+						borderColor: '#ff7f0e',
 						fill: false,
+						showLine: true,
 						lineTension: 0,
 						data: [
 							{x:0,y:20},
@@ -77,14 +81,87 @@ export class ClimateChartComponent implements AfterViewInit,OnInit {
 							{x:2,y:19.8},
 							{x:3,y:21.2},
 							{x:4,y:20.8}
-						]
+						],
+						yAxisID: 'y-temperature'
+					},
+					{
+						label: 'Temperature Aux 0',
+						fill: false,
+						borderColor: '#2ca02c',
+						showLine: true,
+						lineTension: 0,
+						data: [
+							{x:0,y:20.1},
+							{x:1,y:20.2},
+							{x:2,y:20.0},
+							{x:3,y:21.3},
+							{x:4,y:20.6}
+						],
+						yAxisID: 'y-temperature'
+					},
+					{
+						label: 'Temperature Aux 1',
+						borderColor: '#17becf',
+						fill: false,
+						showLine: true,
+						lineTension: 0,
+						data: [
+							{x:0,y:21},
+							{x:1,y:21.3},
+							{x:2,y:20.8},
+							{x:3,y:22.2},
+							{x:4,y:21.8}
+						],
+						yAxisID: 'y-temperature'
+					},
+					{
+						label: 'Temperature Aux 2',
+						borderColor: '#ff6384',
+						fill: false,
+						showLine: true,
+						lineTension: 0,
+						data: [
+							{x:0,y:20.5},
+							{x:1,y:20.9},
+							{x:2,y:20.3},
+							{x:3,y:21.6},
+							{x:4,y:21.2}
+						],
+						yAxisID: 'y-temperature'
 					}
 				],
 			},
 			options: {
 				responsive: false,
+				legend: {position: 'bottom'},
+				stacked: false,
+				hoverMode: 'index',
+				scales: {
+					xAxes: [
+						{
+							scaleLabel:{display: true,labelString: 'Time (m)'},
+							display: true,
+						}
+					],
+					yAxes:[
+						{
+							type: 'linear',
+							display: true,
+							position: 'right',
+							id: 'y-humidity',
+							gridLines: { drawOnChartArea: false},
+							scaleLabel:{display: true,labelString: 'Humidity (%)'},
+						},
+						{
+							type: 'linear',
+							display: true,
+							position: 'left',
+							id: 'y-temperature',
+							scaleLabel:{display: true,labelString: 'Temperature (°C)'},
+						}
+					]
+				}
 			}
-
 		});
 
         //todo display a chart
