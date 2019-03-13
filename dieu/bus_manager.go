082433754426e64997e47013b478d12ca77ba728
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-03-13 21:47:08 +0100

    debug

--- dieu/bus_manager.go
+++ dieu/bus_manager.go
@@ -99,6 +99,7 @@ func (b *busManager) Listen() {
 					b.callbackWaitGroup.Add(1)
 					go func(m *StampedMessage, alarms chan<- dieu.Alarm) {
 						for _, callback := range callbacks {
+							log.Printf("callback %+v", callback)
 							callback(alarms, m)
 						}
 						b.callbackWaitGroup.Done()
