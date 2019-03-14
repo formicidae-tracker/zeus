7813c88ec67d09cd8813200af95930f92c40dff1
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-03-14 18:31:12 +0100

    Implements state components

--- olympus/webapp/src/app/state/state.component.ts
+++ olympus/webapp/src/app/state/state.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { State }  from '../core/state.model';
+
 
 @Component({
-  selector: 'app-state',
-  templateUrl: './state.component.html',
-  styleUrls: ['./state.component.css']
+    selector: 'app-state',
+    templateUrl: './state.component.html',
+    styleUrls: ['./state.component.css']
 })
 export class StateComponent implements OnInit {
+    @Input() state: State;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
