9796293ae867775d34322f10b4ba2d9fb8d02af8
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-01-25 22:20:16 +0100

    Reduces number of opened timers and spawned go routines

--- zeus/alarm_monitor.go
+++ zeus/alarm_monitor.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 	"path"
-	"sort"
 	"time"
 
 	"github.com/formicidae-tracker/zeus"
@@ -26,57 +25,48 @@ func (m *alarmMonitor) Name() string {
 	return m.name
 }
 
-type deadline struct {
-	reason string
-	when   time.Time
-}
-
-type deadlineList []deadline
-
-func (l deadlineList) Len() int {
-	return len(l)
-}
-func (l deadlineList) Swap(i, j int) {
-	l[i], l[j] = l[j], l[i]
-}
-func (l deadlineList) Less(i, j int) bool {
-	return l[i].when.Before(l[j].when)
-}
-
 type deadlineMeeter struct {
-	deadlines []deadline
+	deadlines map[string]time.Time
 }
 
-func (d *deadlineMeeter) pushDeadline(reason string, after time.Duration) <-chan time.Time {
-	now := time.Now()
-	d.deadlines = append(d.deadlines, deadline{reason: reason, when: now.Add(after)})
-	sort.Sort(deadlineList(d.deadlines))
-	return time.After(d.deadlines[0].when.Sub(now) + 10*time.Millisecond)
-}
-
-func (d *deadlineMeeter) pop(now time.Time) ([]string, <-chan time.Time) {
-	res := make([]string, 0, len(d.deadlines))
-	i := 0
-	deadline := deadline{}
-	for i, deadline = range d.deadlines {
-		if deadline.when.After(now) == true {
-			break
-		}
-		res = append(res, deadline.reason)
-	}
-	d.deadlines = d.deadlines[i:]
+func (d *deadlineMeeter) next(now time.Time) *time.Timer {
 	if len(d.deadlines) == 0 {
-		return res, nil
+		return nil
 	}
-	return res, time.After(d.deadlines[0].when.Sub(now) + 10*time.Millisecond)
+	set := false
+	var minTime time.Time
+	for _, when := range d.deadlines {
+		if set == false || when.Before(minTime) == true {
+			minTime = when
+			set = true
+		}
+	}
+
+	return time.NewTimer(minTime.Sub(now))
+}
+
+func (d *deadlineMeeter) pushDeadline(reason string, after time.Duration) *time.Timer {
+	now := time.Now()
+	d.deadlines[reason] = now.Add(after)
+	return d.next(now)
+}
+
+func (d *deadlineMeeter) pop(now time.Time) ([]string, *time.Timer) {
+	res := make([]string, 0, len(d.deadlines))
+	for reason, when := range d.deadlines {
+		if when.After(now) == true {
+			continue
+		}
+		res = append(res, reason)
+	}
+	for _, reason := range res {
+		delete(d.deadlines, reason)
+	}
+	return res, d.next(now)
 }
 
 func (m *alarmMonitor) Monitor() {
-	type alarmData struct {
-		alarm zeus.Alarm
-		count int
-	}
-	alarms := make(map[string]*alarmData)
+	alarms := make(map[string]zeus.Alarm)
 
 	defer func() {
 		close(m.outbound)
@@ -84,9 +74,22 @@ func (m *alarmMonitor) Monitor() {
 
 	quit := make(chan struct{})
 
-	meeter := &deadlineMeeter{}
+	meeter := &deadlineMeeter{make(map[string]time.Time)}
 
-	var wakeUp <-chan time.Time = nil
+	var wakeUpTimer *time.Timer = nil
+	var wakeUpChan <-chan time.Time = nil
+
+	pushTimer := func(newTimer *time.Timer) {
+		if wakeUpTimer != nil {
+			wakeUpTimer.Stop()
+		}
+		wakeUpTimer = newTimer
+		if wakeUpTimer == nil {
+			wakeUpChan = nil
+		} else {
+			wakeUpChan = wakeUpTimer.C
+		}
+	}
 
 	for {
 		select {
@@ -95,7 +98,7 @@ func (m *alarmMonitor) Monitor() {
 				close(quit)
 				return
 			}
-			if d, ok := alarms[a.Reason()]; ok == false || d.count <= 0 {
+			if _, ok := alarms[a.Reason()]; ok == false {
 				go func() {
 					m.outbound <- zeus.AlarmEvent{
 						Reason: a.Reason(),
@@ -105,36 +108,30 @@ func (m *alarmMonitor) Monitor() {
 						Zone:   m.name,
 					}
 				}()
-				alarms[a.Reason()] = &alarmData{alarm: a, count: 1}
-			} else {
-				alarms[a.Reason()].count += 1
+
 			}
-			wakeUp = meeter.pushDeadline(a.Reason(), a.DeadLine())
-		case now := <-wakeUp:
-			var expired []string = nil
-			expired, wakeUp = meeter.pop(now)
+			alarms[a.Reason()] = a
+			pushTimer(meeter.pushDeadline(a.Reason(), a.DeadLine()))
+		case now := <-wakeUpChan:
+			expired, newTimer := meeter.pop(now)
+			pushTimer(newTimer)
+
 			for _, r := range expired {
 				a, ok := alarms[r]
 				if ok == false {
 					// should not happen but lets says it does
 					continue
 				}
-
-				if a.count == 1 {
-					go func() {
-						m.outbound <- zeus.AlarmEvent{
-							Reason: a.alarm.Reason(),
-							Flags:  a.alarm.Flags(),
-							Status: zeus.AlarmOff,
-							Time:   now,
-							Zone:   m.name,
-						}
-					}()
-					delete(alarms, r)
-				}
-				if a.count > 1 {
-					a.count -= 1
-				}
+				go func() {
+					m.outbound <- zeus.AlarmEvent{
+						Reason: a.Reason(),
+						Flags:  a.Flags(),
+						Status: zeus.AlarmOff,
+						Time:   now,
+						Zone:   m.name,
+					}
+				}()
+				delete(alarms, r)
 			}
 		}
 	}
