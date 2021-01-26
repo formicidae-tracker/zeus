70f99d29fbfda73a480b85834ad9537a1c8e0dd2
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-01-26 23:45:43 +0100

    Fixes scan

--- zeus-cli/scan.go
+++ zeus-cli/scan.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/formicidae-tracker/zeus"
 )
@@ -10,32 +11,38 @@ type ScanCommand struct {
 }
 
 func (c *ScanCommand) Execute(args []string) error {
+	now := time.Now()
 	nodes, err := Nodes()
 	if err != nil {
 		return err
 	}
-	format := " %15s | %-7s | %-25s | %-10s \n"
-	fmt.Printf(format, "Node", "Status", "Version", "Compatible")
-	fmt.Println("-----------------+---------+---------------------------+------------")
+	format := " %15s | %-7s | %-11s | %-20s | %-10s \n"
+	fmt.Printf(format, "Node", "Status", "Since", "Version", "Compatible")
+	fmt.Println("-----------------+---------+-------------+----------------------+------------")
 
 	for _, node := range nodes {
 		status := zeus.ZeusStatusReply{}
 		ignored := 0
 		err := node.RunMethod("Zeus.Status", ignored, &status)
 		if err != nil {
-			return err
+			fmt.Printf(format, node.Name, "n.a.", "n.a.", "<v0.3.0", "✗")
+			continue
 		}
 		statusValue := "Idle"
+		sinceValue := "n.a."
 		if status.Running == true {
 			statusValue = "Running"
+			ellapsed := now.Sub(status.Since).Truncate(time.Second)
+			sinceValue = ellapsed.String()
 		}
+
 		compatibleValue := "✓"
 		compatible, err := zeus.VersionAreCompatible(zeus.ZEUS_VERSION, status.Version)
 		if err != nil || compatible == false {
 			compatibleValue = "✗"
 		}
 
-		fmt.Printf(format, node.Name, statusValue, status.Version, compatibleValue)
+		fmt.Printf(format, node.Name, statusValue, sinceValue, status.Version, compatibleValue)
 	}
 	return nil
 }
