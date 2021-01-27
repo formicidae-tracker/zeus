1d36c754d2e782e3ffc0d116743469ea441f7b9e
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-01-27 11:42:16 +0100

    Reads climate log files

--- zeus/climate_reporter.go
+++ zeus/climate_reporter.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -66,7 +69,94 @@ func NewFileClimateReporter(filename string, numAux int) (ClimateReporter, strin
 		header += fmt.Sprintf(" Aux %d (°C)", i+1)
 	}
 
-	fmt.Fprintf(res.File, "# Starting date %s\n%s\n", res.Start, header)
+	fmt.Fprintf(res.File, "# Starting date %s\n%s\n", res.Start.Format(time.RFC3339Nano), header)
 
 	return res, fname, nil
+}
+
+func readStartDate(r *bufio.Reader) (time.Time, error) {
+	l, err := r.ReadString('\n')
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	l = strings.TrimPrefix(l, "# Starting date")
+	l = strings.TrimSpace(l)
+	return time.Parse(time.RFC3339Nano, l)
+}
+
+func readNumAux(r *bufio.Reader) (int, error) {
+	l, err := r.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	res := strings.Count(l, "(°C)") - 1
+	if res < 0 {
+		return 0, fmt.Errorf("invalid header '%s'", strings.TrimSpace(l))
+	}
+	return res, nil
+}
+
+func readClimateReport(r *bufio.Reader, start time.Time, numAux int) (zeus.ClimateReport, error) {
+	res := zeus.ClimateReport{}
+	l, err := r.ReadString('\n')
+	if err != nil {
+		return res, err
+	}
+	l = strings.TrimSpace(l)
+	valuesStr := strings.Split(l, " ")
+	if len(valuesStr) < 3+numAux {
+		return res, fmt.Errorf("invalid line '%s': too few values", l)
+	}
+	ms, err := strconv.ParseInt(valuesStr[0], 10, 64)
+	if err != nil {
+		return res, fmt.Errorf("invalid timestamp '%s': %s", valuesStr[0], err)
+	}
+	res.Time = start.Add(time.Duration(ms) * time.Millisecond)
+	h, err := strconv.ParseFloat(valuesStr[1], 64)
+	if err != nil {
+		return res, fmt.Errorf("invalid humidity '%s': %s", valuesStr[1], err)
+	}
+	res.Humidity = zeus.Humidity(h)
+	res.Temperatures = make([]zeus.Temperature, numAux+1)
+	for i, tStr := range valuesStr[2:(numAux + 3)] {
+		t, err := strconv.ParseFloat(tStr, 64)
+		if err != nil {
+			return res, fmt.Errorf("invalid temperature '%s': %s", tStr, err)
+		}
+		res.Temperatures[i] = zeus.Temperature(t)
+	}
+	return res, nil
+}
+
+func ReadClimateFile(filename string) ([]zeus.ClimateReport, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+	startDate, err := readStartDate(reader)
+	if err != nil {
+		return nil, err
+	}
+	numAux, err := readNumAux(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	var res []zeus.ClimateReport
+	for {
+		cr, err := readClimateReport(reader, startDate, numAux)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return res, err
+		}
+		res = append(res, cr)
+	}
+
+	return res, nil
 }
--- zeus/file_climate_reporter_test.go
+++ zeus/file_climate_reporter_test.go
@@ -71,6 +71,84 @@ func (s *FileClimateReporterSuite) TestFileNameWriting(c *C) {
 333 50.00 21.00 21.00 21.00 21.00
 666 50.00 21.00 21.00 21.00 21.00
 999 50.00 21.00 21.00 21.00 21.00
-`, fn.(*fileClimateReporter).Start))
+`, fn.(*fileClimateReporter).Start.Format(time.RFC3339Nano)))
 
+}
+
+func (s *FileClimateReporterSuite) TestFileReading(c *C) {
+	tmpdir, err := ioutil.TempDir("", "zeus-read-season-file")
+	c.Assert(err, IsNil)
+	defer os.RemoveAll(tmpdir)
+	start := time.Now().Round(0) // removes monotonic clocks value for deep equals
+	startString := start.Format(time.RFC3339Nano)
+	testdata := []struct {
+		Content  string
+		Expected []zeus.ClimateReport
+		Error    string
+	}{
+		{
+			Content: "# Starting date " + startString + `
+# Time (ms) Relative Humidity (%) Temperature (°C)
+0 50.0 21.23
+502 51.3 24.5
+`,
+			Expected: []zeus.ClimateReport{
+				zeus.ClimateReport{Time: start, Humidity: 50.0, Temperatures: []zeus.Temperature{21.23}},
+				zeus.ClimateReport{Time: start.Add(502 * time.Millisecond), Humidity: 51.3, Temperatures: []zeus.Temperature{24.5}},
+			},
+		},
+		{
+			Content: "# Starting date " + startString + `
+# Time (ms) Relative Humidity (%) Temperature (°C) Aux 1 (°C)
+0 50.0 21.23 13.2
+502 51.3 24.5 15.7
+`,
+			Expected: []zeus.ClimateReport{
+				zeus.ClimateReport{Time: start, Humidity: 50.0, Temperatures: []zeus.Temperature{21.23, 13.2}},
+				zeus.ClimateReport{Time: start.Add(502 * time.Millisecond), Humidity: 51.3, Temperatures: []zeus.Temperature{24.5, 15.7}},
+			},
+		},
+		{
+			Content: "# Starting date " + startString + `fo
+# Time (ms) Relative Humidity (%) Temperature (°C) Aux 1 (°C)
+0 50.0 21.23 13.2
+502 51.3 24.5 15.7
+`,
+			Error: "parsing time \".*\": extra text: fo",
+		},
+		{
+			Content: "# Starting date " + startString + `
+# Time (ms) Relative Humidity (%)
+0 50.0 21.23 13.2
+502 51.3 24.5 15.7
+`,
+			Error: "invalid header .*",
+		},
+		{
+			Content: "# Starting date " + startString + `
+# Time (ms) Relative Humidity (%) Temperature (°C) Aux 1 (°C) Aux 2 (°C)
+0 50.0 21.23 13.2 34.1
+502 51.3 24.5 15.7
+`,
+			Error: "invalid line .*: too few values",
+			Expected: []zeus.ClimateReport{
+				zeus.ClimateReport{Time: start, Humidity: 50.0, Temperatures: []zeus.Temperature{21.23, 13.2, 34.1}},
+			},
+		},
+	}
+	filename := filepath.Join(tmpdir, "log.txt")
+	for _, d := range testdata {
+		err := ioutil.WriteFile(filename, []byte(d.Content), 0644)
+		if c.Check(err, IsNil) == false {
+			continue
+		}
+		result, err := ReadClimateFile(filename)
+		if len(d.Error) > 0 {
+			c.Check(err, ErrorMatches, d.Error)
+		} else {
+			c.Check(err, IsNil)
+		}
+		c.Check(result, DeepEquals, d.Expected)
+
+	}
 }
